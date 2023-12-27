a59e639a41f626fb82931f4e12de89c9d7003dd7
julian <julian.bottiglione@epitech.eu>
2023-12-27 10:45:54 +0100

    [fix] Replaced type with uintX_t

--- backEnd/library/includes/pokemon_file.h
+++ backEnd/library/includes/pokemon_file.h
@@ -5,90 +5,92 @@
 ** pokemon_file
 */
 
+#include <stdint.h>
+
 typedef struct pokemon_file_s( scalar_storage_order("big-endian") )
 {
     /*          "Header"          */
 
-    unsigned short gift_type;   /*  00 01 for pokemon   */
-    unsigned char card_slot;    /*  from Project Pokemon documentation  */
-    unsigned char data_specific;    /*  from Project Pokemon documentation  */
-    unsigned int a;             /*  Unknow  */
-    unsigned int nature_shinyness;   /*  */
-    unsigned short b;           /*  Unknow  */
-    unsigned short checksum;    /*  Unknow  */
+    uint16_t gift_type;   /*  00 01 for pokemon   */
+    uint8_t card_slot;    /*  from Project Pokemon documentation  */
+    uint8_t data_specific;    /*  from Project Pokemon documentation  */
+    uint32_t a;             /*  Unknow  */
+    uint32_t nature_shinyness;   /*  */
+    uint16_t b;           /*  Unknow  */
+    uint16_t checksum;    /*  Unknow  */
 
     /*          Pokemon Data            */
-    unsigned short dex_number;
-    unsigned short item_held;
-    unsigned short TID;
-    unsigned short SID;
-    unsigned int   experience_point;
-    unsigned char friendship;
-    unsigned char ability;
-    unsigned char marking;
-    unsigned char language_of_origin;
+    uint16_t dex_number;
+    uint16_t item_held;
+    uint16_t TID;
+    uint16_t SID;
+    uint32_t experience_point;
+    uint8_t friendship;
+    uint8_t ability;
+    uint8_t marking;
+    uint8_t language_of_origin;
 
     /*          Effort Values           */
-    unsigned char hp_ev;
-    unsigned char attack_ev;
-    unsigned char defence_ev;
-    unsigned char speed_ev;
-    unsigned char sp_attack_ev;
-    unsigned char sp_defence_ev;
+    uint8_t hp_ev;
+    uint8_t attack_ev;
+    uint8_t defence_ev;
+    uint8_t speed_ev;
+    uint8_t sp_attack_ev;
+    uint8_t sp_defence_ev;
     /*          Contest Values          */
-    unsigned char cool;
-    unsigned char beauty;
-    unsigned char cute;
-    unsigned char smart;
-    unsigned char tough;
-    unsigned char sheen;
+    uint8_t cool;
+    uint8_t beauty;
+    uint8_t cute;
+    uint8_t smart;
+    uint8_t tough;
+    uint8_t sheen;
 
     /*          Sinnoh Ribbons 1        */
-    unsigned int sinnoh_ribbons_1;  /*  see sinnoh_ribbons_1.h    */
+    uint32_t sinnoh_ribbons_1;  /*  see sinnoh_ribbons_1.h    */
     
     /*          Moves Values            */
-    unsigned short move1;
-    unsigned short move2;
-    unsigned short move3;
-    unsigned short move4;
+    uint16_t move1;
+    uint16_t move2;
+    uint16_t move3;
+    uint16_t move4;
 
-    unsigned char pp1;
-    unsigned char pp2;
-    unsigned char pp3;
-    unsigned char pp4;
+    uint8_t pp1;
+    uint8_t pp2;
+    uint8_t pp3;
+    uint8_t pp4;
     
-    unsigned char pp_up1;
-    unsigned char pp_up2;
-    unsigned char pp_up3;
-    unsigned char pp_up4;
+    uint8_t pp_up1;
+    uint8_t pp_up2;
+    uint8_t pp_up3;
+    uint8_t pp_up4;
 
-    unsigned int individual_value;  /*  Bit 30 = Egg flag;
+    uint32_t individual_value;  /*  Bit 30 = Egg flag;
                                         Bit 31 = Nickname flag   */
 
     /*          Hoenn Ribbons           */
-    unsigned int hoenn_ribbons;     /*  see hoenn_ribbons.h     */
+    uint32_t hoenn_ribbons;     /*  see hoenn_ribbons.h     */
 
-    unsigned char form;             /* Bit 0 = Fateful encounter;
+    uint8_t form;             /* Bit 0 = Fateful encounter;
                                        Bit 1&2 = Gender;
                                        Bit 3-7 = Alternate forms */
-    unsigned char shiny_leaves;
+    uint8_t shiny_leaves;
 
-    unsigned short unused;
-    unsigned short platinum_egg_location;
-    unsigned short platinum_met_location;
+    uint16_t unused;
+    uint16_t platinum_egg_location;
+    uint16_t platinum_met_location;
 
     /*          Nickname                */
-    unsigned short nickname[11];    /* see character_encoding.h */
+    uint16_t nickname[11];    /* see character_encoding.h */
     
-    unsigned char unused;
+    uint8_t unused;
 
-    unsigned char origin_game;
+    uint8_t origin_game;
 
     /*          Sinnoh Ribbons 2         */
-    unsigned int sinnoh_ribbons_2;  /* see sinnoh_ribbons_2.h   */
+    uint32_t sinnoh_ribbons_2;  /* see sinnoh_ribbons_2.h   */
 
-    unsigned int unused2;
+    uint32_t unused2;
     
     /*          Trainer Name             */
-    unsigned short trainer_name[8];
+    uint16_t trainer_name[8];
 } pokemon_file_t;
